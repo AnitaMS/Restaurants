447f8e0191d51e2225a5db06fb436700ddeca052
anita <anita@anita-X550LD>
2017-03-13 15:30:29 +0100

    option excluded2

--- smoke/src/test/java/com/restaurants/smoke/Tests.java
+++ smoke/src/test/java/com/restaurants/smoke/Tests.java
@@ -33,7 +33,7 @@ public class Tests {
 
 	@BeforeClass(alwaysRun = true)
 	@Parameters("browserType")
-	public void beforeClass(@Optional String browser) {
+	public void beforeClass(String browser) {
 
 		if (browser.equalsIgnoreCase("firefox")) {
 			System.setProperty("webdriver.gecko.driver", "/home/anita/Desktop/geckodriver/geckodriver");
