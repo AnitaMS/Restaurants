410dc6e6050c7895c80e14c9df6d93c40f3d02f1
anita <anita@anita-X550LD>
2017-03-14 16:32:27 +0100

    tt

--- smoke/src/test/java/com/restaurants/smoke/Tests.java
+++ smoke/src/test/java/com/restaurants/smoke/Tests.java
@@ -29,7 +29,7 @@ public class Tests {
 	Restaurants restaurants;
 	final String randomEmail = null;
 	///ExtentReports report;
-	////7ExtentTest test;
+	////77ExtentTest test;
 
 	@BeforeClass(alwaysRun = true)
 	@Parameters( "browserType")
