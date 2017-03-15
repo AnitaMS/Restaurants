5ef110f7b26306db4b1e9afc3d5f7ad727ecd418
anita <anita@anita-X550LD>
2017-03-15 15:15:59 +0100

    pp

--- smoke/src/test/java/com/restaurants/smoke/Tests.java
+++ smoke/src/test/java/com/restaurants/smoke/Tests.java
@@ -37,7 +37,7 @@ public class Tests {
 			System.setProperty("webdriver.gecko.driver", "/home/anita/Desktop/geckodriver/geckodriver");
 			 driver = new FirefoxDriver();
 
-			//DesiredCapabilities cap = DesiredCapabilities.firefox();
+			////DesiredCapabilities cap = DesiredCapabilities.firefox();
 			//cap.setCapability("marionette", true);
 
 			//driver = new FirefoxDriver(cap);
