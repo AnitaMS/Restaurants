393c3dc6f1dd9cd4a1327f4bafaa7609dc162a05
anita <anita@anita-X550LD>
2017-03-22 11:56:29 +0100

    yxyx

--- smoke/src/test/java/com/restaurants/smoke/Screenshots.java
+++ smoke/src/test/java/com/restaurants/smoke/Screenshots.java
@@ -12,7 +12,7 @@ public class Screenshots {
 	///77fff
 	public static String takeScreenshot(WebDriver driver, String fileName) throws IOException {
 		fileName = fileName + ".png";
-		String directory = "smoke/resources/";
+		String directory = "smoke/reports/";
 		File sourceFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(sourceFile, new File(directory + fileName));
 		String destination = directory + fileName;
