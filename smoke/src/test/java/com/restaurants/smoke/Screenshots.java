0665aad735b5622cc1dd554bd7463b3a4fcf2251
anita <anita@anita-X550LD>
2017-03-23 16:42:20 +0100

    gbg

--- smoke/src/test/java/com/restaurants/smoke/Screenshots.java
+++ smoke/src/test/java/com/restaurants/smoke/Screenshots.java
@@ -15,7 +15,7 @@ public class Screenshots {
 		
 		//String directory = "reports/";
 		
-		String directory = "reports/";
+		String directory = "/smoke/reports";
 		System.out.println(directory.toString() + fileName.toString());
 		File sourceFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(sourceFile, new File(directory + fileName));
