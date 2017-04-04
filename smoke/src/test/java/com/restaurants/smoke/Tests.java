f0116b3bc18a9c3c6964d3af0076617e42ceb42c
Your Name <you@example.com>
2017-04-04 17:04:15 +0200

    yy

--- smoke/src/test/java/com/restaurants/smoke/Tests.java
+++ smoke/src/test/java/com/restaurants/smoke/Tests.java
@@ -43,9 +43,9 @@ public class Tests {
 		
 			// ExtentReports("/home/anita/Desktop/ReportFireFox.html");
 
-			//report = new ExtentReports("/home/anita/git/Restaurants/smoke/reports/ReportFirefox.html");
+			report = new ExtentReports("/home/anita/git/Restaurants/smoke/reports/ReportFirefox.html");
 			
-			report = new ExtentReports("/home/anita/.jenkins/workspace/Restaurant App Smoke Test/smoke/reports/ReportFirefox.html");
+			//report = new ExtentReports("/home/anita/.jenkins/workspace/Restaurant App Smoke Test/smoke/reports/ReportFirefox.html");
 			
 			Map<String, String> sysInfo = new HashMap<String, String>();
 			sysInfo.put("Selenium Version", "3.0.1");
@@ -65,10 +65,10 @@ public class Tests {
 
 			
 			
-		//	report = new ExtentReports("/home/anita/git/Restaurants/smoke/reports/ReportChrome.html");
+			report = new ExtentReports("/home/anita/git/Restaurants/smoke/reports/ReportChrome.html");
 			
 			
-			report = new ExtentReports("/home/anita/.jenkins/workspace/Restaurant App Smoke Test/smoke/reports/ReportChrome.html");
+			//report = new ExtentReports("/home/anita/.jenkins/workspace/Restaurant App Smoke Test/smoke/reports/ReportChrome.html");
 			
 			Map<String, String> sysInfo = new HashMap<String, String>();
 			sysInfo.put("Selenium Version", "3.0.1");
